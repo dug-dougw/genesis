e3e65f9cbb8ff3a798999b4c99986f01dd6f6ef5
Doug Walker <dougw@dug.com>
2022-09-25 13:31:32 +0800

    formatting

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 
 # Install a pre-compiled binary of "just" into /tmp directory
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /tmp
-echo 'Now run "/tmp/just install'
+printf '\nNow run "/tmp/just install\n'
